11344207c457ecf1c73a50c16e762f8d70463b9f
MayderC <maydercortes1@gmail.com>
2024-02-12 08:20:33 -0600

    Remove unused import and add gsap dependency

--- src/components/SolarSystem/SolarSystem.vue
+++ src/components/SolarSystem/SolarSystem.vue
@@ -4,23 +4,49 @@
   </Suspense>
 </template>
 <script lang="ts" setup>
-import { useLoader, useRenderLoop } from '@tresjs/core'
-import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
-import { SOLAR_S, orbitalSpeeds } from '../Three/constants'
-import { ref } from 'vue'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
+import { useLoader, useRenderLoop, useTresContext } from '@tresjs/core'
+import { SOLAR_S, orbitalSpeeds } from '../../Three/constants'
+import { ref, defineEmits } from 'vue'
+import * as THREE from 'three'
 
-console.log('SolarSystem.vue')
-console.log('useLoader', useLoader)
-console.log('GLTFLoader', GLTFLoader)
+const emitter = defineEmits(['onLoaded'])
+const { camera, renderer, scene } = useTresContext()
 
-const glb = await useLoader(GLTFLoader, '/spacev2.glb')
+const glb = await useLoader(GLTFLoader, '/space-origin.glb')
 
 glb.scene.position.set(0, 0, 0)
+glb.scene.scale.set(0.7, 0.7, 0.7)
 
-glb.scene.scale.set(0.5, 0.5, 0.5)
+const postprocessing = () => {
+  const renderPass = new RenderPass(scene.value, camera.value!)
+  const bloomPass = new UnrealBloomPass(
+    new THREE.Vector2(window.innerWidth, window.innerHeight),
+    1.5,
+    0.4,
+    0.85
+  )
 
-const { onLoop, resume } = useRenderLoop()
+  bloomPass.threshold = 0
+  bloomPass.strength = 1
+  bloomPass.radius = 0
+  bloomPass.renderToScreen = true
+
+  const composer = new EffectComposer(renderer.value)
+  composer.setSize(window.innerWidth, window.innerHeight)
+  composer.addPass(renderPass)
+  composer.addPass(bloomPass)
+  renderer.value.toneMappingExposure = Math.pow(0.9, 4.0)
+
+  return composer
+}
+
+const composer = postprocessing()
+
+const { onLoop, pause, resume } = useRenderLoop()
 
 const earth = glb.scene.getObjectByName(SOLAR_S.Earth_Icosphere) as THREE.Mesh
 const moon = glb.scene.getObjectByName(SOLAR_S.Moon_Icosphere001) as THREE.Mesh
@@ -39,10 +65,11 @@ const saturnGroup = new THREE.Group()
 saturnGroup.add(saturn, saturnRing)
 earthGroup.add(earth, moon)
 
-const canIRotate = ref(true)
+const canIRotate = ref(false)
 
-onLoop(({ delta, elapsed, clock }) => {
-  if (!canIRotate.value) return
+onLoop(({ elapsed }) => {
+  //composer.render()
+  if (canIRotate.value == false) return
 
   moon.rotation.y = -orbitalSpeeds[moon.name] * elapsed
   earthGroup.rotation.y = -orbitalSpeeds[earth.name] * elapsed
@@ -55,5 +82,8 @@ onLoop(({ delta, elapsed, clock }) => {
   jupiter.rotation.z = orbitalSpeeds[jupiter.name] * elapsed
 })
 
-glb.scene.add(earthGroup, saturnGroup)
+glb.scene.add(earthGroup)
+glb.scene.add(saturnGroup)
+
+emitter('onLoaded', { scene: glb.scene, camera: camera.value! })
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,198 @@
 <script lang="ts" setup>
 import { TresCanvas, useRenderLoop } from '@tresjs/core'
-import { Sky, Stars, OrbitControls } from '@tresjs/cientos'
-
-import SolarSystem from '@/components/SolarSystem.vue'
+import { Stars, OrbitControls } from '@tresjs/cientos'
+import SolarSystem from '@/components/SolarSystem/SolarSystem.vue'
 import { ref } from 'vue'
+import { gsap } from 'gsap'
+import { SOLAR_S } from '../Three/constants'
 
-const { onLoop, resume } = useRenderLoop()
+interface EmitArgs {
+  scene: THREE.Group
+  camera: THREE.PerspectiveCamera
+}
 
-const mesh = ref<THREE.Mesh>()
+const { onLoop, pause } = useRenderLoop()
+const glbScene = ref<THREE.Group>()
+const tresCamera = ref<THREE.PerspectiveCamera>()
+const lookAt = ref({ x: 0, y: 0, z: 0 })
 const yRotation = ref(0)
 
-onLoop(({ delta, elapsed, clock }) => {
-  yRotation.value = elapsed / 10
-})
+onLoop(({ elapsed }) => (yRotation.value = elapsed / 10))
+
+const onLoaded = ({ scene, camera }: EmitArgs) => {
+  glbScene.value = scene
+  tresCamera.value = camera
+  scrollRotate()
+}
+
+const scrollRotate = () => {
+  window.addEventListener('scroll', () => {
+    const scrollPercent =
+      Math.floor((window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100) / 40
+    const rotationValue = (scrollPercent * 360) / 100
+
+    if (scrollPercent * 40 < 22) {
+      gsap.to(glbScene.value!.rotation, {
+        x: Math.PI * 2,
+        y: rotationValue,
+        duration: 2,
+        ease: 'power4.out'
+      })
+
+      gsap.to(tresCamera.value?.position!, {
+        x: 0,
+        y: 0,
+        z: 75,
+        duration: 0.5,
+        ease: 'power4.out',
+        onComplete: () => {
+          gsap.to(lookAt.value!, {
+            x: 0,
+            y: 0,
+            z: 0,
+            duration: 0.5,
+            ease: 'power4.out',
+            onComplete: () => {
+              tresCamera.value!.updateProjectionMatrix()
+            }
+          })
+        }
+      })
+    }
+
+    if (scrollPercent * 40 >= 22) {
+      gsap.to(glbScene.value!.rotation, {
+        x: Math.PI / 2,
+        y: 1,
+        duration: 2,
+        ease: 'power4.out'
+      })
+
+      const mercury = glbScene.value!.getObjectByName(SOLAR_S.Mercury_Icosphere005) as THREE.Mesh
+      gsap.to(tresCamera.value?.position!, {
+        x: mercury.position.x,
+        y: 50,
+        z: mercury.position.z,
+        duration: 0.5,
+        ease: 'power4.out',
+        onComplete: () => {
+          gsap.to(lookAt.value!, {
+            x: 0,
+            y: 0,
+            z: 0,
+            duration: 0.5,
+            ease: 'power4.out',
+            onComplete: () => {
+              tresCamera.value!.updateProjectionMatrix()
+            }
+          })
+        }
+      })
+    }
+
+    if (scrollPercent * 40 == 22) {
+      const mercury = glbScene.value!.getObjectByName(SOLAR_S.Mercury_Icosphere005) as THREE.Mesh
+      gsap.to(tresCamera.value?.position!, {
+        x: mercury.position.x,
+        y: 50,
+        z: mercury.position.z,
+        duration: 0.5,
+        ease: 'power4.out',
+        onComplete: () => {
+          gsap.to(lookAt.value!, {
+            x: 0,
+            y: 0,
+            z: 0,
+            duration: 0.5,
+            ease: 'power4.out',
+            onComplete: () => {
+              tresCamera.value!.updateProjectionMatrix()
+            }
+          })
+        }
+      })
+    }
+
+    if (scrollPercent * 40 == 33) {
+      const venus = glbScene.value!.getObjectByName(SOLAR_S.Venus_Icosphere005) as THREE.Mesh
+
+      gsap.to(glbScene.value!.rotation, {
+        x: Math.PI / 2,
+        y: -0.5,
+        duration: 2,
+        ease: 'power4.out'
+      })
+    }
+
+    if (scrollPercent * 40 == 44) {
+      gsap.to(glbScene.value!.rotation, {
+        x: Math.PI / 2,
+        y: -3.5,
+        duration: 2,
+        ease: 'power4.out'
+      })
+    }
+
+    if (scrollPercent * 40 == 55) {
+      gsap.to(glbScene.value!.rotation, {
+        x: Math.PI / 2 - 0.05,
+        y: -5.6,
+        duration: 2,
+        ease: 'power4.out'
+      })
+    }
+
+    if (scrollPercent * 40 == 66) {
+      gsap.to(glbScene.value!.rotation, {
+        x: Math.PI / 2 - 0.1,
+        y: -7.94,
+        duration: 2,
+        ease: 'power4.out'
+      })
+    }
+
+    if (scrollPercent * 40 == 77) {
+      const saturn = glbScene.value!.getObjectByName(SOLAR_S.Saturn_Icosphere005) as THREE.Mesh
+
+      gsap.to(glbScene.value!.rotation, {
+        x: Math.PI / 2 - 0.1,
+        y: -9.02,
+        duration: 2,
+        ease: 'power4.out'
+      })
+    }
+
+    if (scrollPercent * 40 == 88) {
+      gsap.to(glbScene.value!.rotation, {
+        x: Math.PI / 2 - 0.1,
+        y: -10.7,
+        duration: 2,
+        ease: 'power4.out'
+      })
+    }
+
+    if (scrollPercent * 40 == 100) {
+      gsap.to(glbScene.value!.rotation, {
+        x: Math.PI / 2 - 0.01,
+        y: -12.6,
+        duration: 2,
+        ease: 'power4.out'
+      })
+    }
+
+    console.log(scrollPercent * 40)
+  })
+}
 </script>
 
 <template>
-  <TresCanvas clear-color="#1a1a1a" window-size :antialias="true">
-    <TresPerspectiveCamera :fov="30" :position="[0, 0, 75]" :look-at="[0, 0, 0]" />
-    <TresAmbientLight :intensity="1" />
+  <TresCanvas class="canvas" clear-color="#1a1a1a" window-size :antialias="true">
+    <TresAmbientLight :intensity="2" />
+    <TresPerspectiveCamera
+      :fov="30"
+      :position="[0, 0, 75]"
+      :look-at="(() => [lookAt.x, lookAt.y, lookAt.z])()"
+    />
     <Stars
       :rotation="[0, yRotation, 0]"
       :radius="50"
@@ -28,12 +202,79 @@ onLoop(({ delta, elapsed, clock }) => {
       :size-attenuation="true"
     />
     <Suspense>
-      <SolarSystem></SolarSystem>
+      <SolarSystem @on-loaded="onLoaded"></SolarSystem>
     </Suspense>
-    <OrbitControls />
-    <TresPointLight :position="[0, 0, 0]" :intensity="5" />
-    <TresAxesHelper :size="100" />
   </TresCanvas>
+
+  <section class="intro">
+    <h1>Solar System</h1>
+    <p>
+      The Solar System is the gravitationally bound system of the Sun and the objects that orbit it,
+    </p>
+  </section>
+  <section class="sun">
+    <h1>Sun</h1>
+    <p>
+      The Sun is the star at the center of the Solar System. It is a nearly perfect sphere of hot
+      plasma.
+    </p>
+  </section>
+  <section class="mercury">
+    <h1>Mercury</h1>
+    <p>
+      Mercury is the smallest and innermost planet in the Solar System. Its orbit around the Sun
+      takes 87.97 Earth days.
+    </p>
+  </section>
+  <section class="venus">
+    <h1>Venus</h1>
+    <p>
+      Venus is the second planet from the Sun. It has a day longer than its year; it rotates
+      backwards compared to most other planets.
+    </p>
+  </section>
+  <section class="earth">
+    <h1>Earth</h1>
+    <p>
+      Earth is the third planet from the Sun and the only astronomical object known to harbor and
+      support life.
+    </p>
+  </section>
+  <section class="mars">
+    <h1>Mars</h1>
+    <p>
+      Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System,
+      being larger than only Mercury.
+    </p>
+  </section>
+  <section class="jupiter">
+    <h1>Jupiter</h1>
+    <p>
+      Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a
+      gas-giant planet.
+    </p>
+  </section>
+  <section class="saturn">
+    <h1>Saturn</h1>
+    <p>
+      Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after
+      Jupiter.
+    </p>
+  </section>
+  <section class="uranus">
+    <h1>Uranus</h1>
+    <p>
+      Uranus is the seventh planet from the Sun. It has the third-largest planetary radius and
+      fourth-largest planetary mass in the Solar System.
+    </p>
+  </section>
+  <section class="neptune">
+    <h1>Neptune</h1>
+    <p>
+      Neptune is the eighth and farthest known Solar planet from the Sun. In the Solar System, it is
+      the fourth-largest planet by diameter.
+    </p>
+  </section>
 </template>
 
 <style>
@@ -41,12 +282,35 @@ html,
 body {
   margin: 0;
   padding: 0;
-  height: 100%;
+  height: 100vh;
   width: 100%;
-  background-color: #1a1a1a;
+  background-color: hsl(0, 0%, 10%);
+  scroll-behavior: smooth;
+
+  scroll-snap-type: y mandatory;
 }
-#app {
+
+section {
+  height: 100vh;
+  scroll-snap-align: start;
+  position: relative;
+  z-index: 1;
+
+  color: white;
+  text-align: center;
+  font-family: 'Roboto', sans-serif;
+  scroll-snap-align: start;
+  font-size: 1.5rem;
+  line-height: 1.5;
+  height: 100vh;
+  width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+.canvas {
   height: 100%;
   width: 100%;
+  position: fixed;
 }
 </style>
